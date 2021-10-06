6af06e93758d01f4fe607034053be57c699f94e8
Сергій Пазюк <89936215+SerhiyPaziyc@users.noreply.github.com>
2021-10-06 18:17:13 +0300

    laba2

--- Source.cpp
+++ Source.cpp
@@ -3,39 +3,26 @@
 // Лабораторна робота № 3.4
 // Розгалуження, задане формулою: функція з параметрами.
 // Варіант 9
-    #include <iostream>
-    #include <cmath>
+#include <cmath>
+#include <iostream>
 
-    using namespace std;
+using namespace std;
 
-    int main()
-    {
+int main()
+{
     double x;
     double y;
+    double r;
 
     cout << "x = "; cin >> x;
+    cout << "y = "; cin >> y;
+    cout << "R = "; cin >> r;
 
-    if (x < -7) {
-        y = 0;
-    }
-    else if (x >= -7 && x <= -3) {
-        y = x + 7;
-    }
-    else if (x > -3 && x < -2) {
-        y = 4;
-    }
-    else if (x >= -2 && x <= 2) {
-        y = x * x;
-    }
-    else if (x > 2 && x < 4) {
-        y = 2 * x - 4;
+    if ((y * y <= r * r - x * x && y <= 0 && x <= 0) || (y * y <= r * r - x * x && y >= pow(x - 1, 2) && x >= 0)) {
+        cout << "Yes";
     }
     else {
-        y = 0;
+        cout << "No";
     }
-
-    cout << "y = " << y;
-
     return 0;
-    }
-
+}
