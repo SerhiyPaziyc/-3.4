83899834d0692a44152e70d5d4cda148568e6722
Сергій Пазюк <89936215+SerhiyPaziyc@users.noreply.github.com>
2021-10-06 17:55:31 +0300

    laba1

--- Source.cpp
+++ Source.cpp
@@ -1,25 +1,41 @@
-// Lab_03_4.cpp
-// Пазюк Сергій Андрійович
+// Lab_03_2.cpp
+// Пазюк Сергій
 // Лабораторна робота № 3.4
-// Розгалуження, задане плоскою фігурою.
+// Розгалуження, задане формулою: функція з параметрами.
 // Варіант 9
-#include <iostream>
-using namespace std;
-int main()
-{
-	double x; // вхідний аргумент
-	double y; // вхідний параметр
-	double R;
-	cout << "x = "; cin >> x;
-	cout << "y = "; cin >> y;
-	cout << "R = "; cin >> R;
+    #include <iostream>
+    #include <cmath>
 
-	// розгалуження в повній формі
-	if ((y<= (x-1)*(x-1) && y>=x) ||
-		(y<= (x-1)*(x-1) && y>=x && y>=R))
-		cout << "yes" << endl;
-	else
-		cout << "no" << endl;
-	cin.get();
-	return 0;
-}
+    using namespace std;
+
+    int main()
+    {
+    double x;
+    double y;
+
+    cout << "x = "; cin >> x;
+
+    if (x < -7) {
+        y = 0;
+    }
+    else if (x >= -7 && x <= -3) {
+        y = x + 7;
+    }
+    else if (x > -3 && x < -2) {
+        y = 4;
+    }
+    else if (x >= -2 && x <= 2) {
+        y = x * x;
+    }
+    else if (x > 2 && x < 4) {
+        y = 2 * x - 4;
+    }
+    else {
+        y = 0;
+    }
+
+    cout << "y = " << y;
+
+    return 0;
+    }
+
